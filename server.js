7492501f632c7501b991771b8a9819a6d415b070
Luis Nunez <103209745+iamluisnunez@users.noreply.github.com>
2023-07-20 18:00:21 -0500

    phase 1 complete

--- server.js
+++ server.js
@@ -1,63 +1,101 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 // Import dependencies
 const express = require("express");
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-
-// Initialize Express
 const app = express();
+app.use(express.static("views"));
 const port = 3000;
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
 
-// Set up MongoDB connection
-mongoose.connect("mongodb://localhost/user-signup-db", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const initializePassPort = require("./passport-config");
+
+initializePassPort(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
+
+const users = [];
+
+app.set("view-enginer", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("index.ejs");
 });
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("login.ejs");
+});
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+
+app.get("/register", checkNotAuthenticated, (req, res) => {
+  res.render("register.ejs");
 });
 
-const User = mongoose.model("User", UserSchema);
-
-// Middleware to parse JSON data
-app.use(express.json());
-
-// Route to handle user signup
-app.post("/signup", async (req, res) => {
+app.post("/register", checkNotAuthenticated, async (req, res) => {
   try {
-    const { username, password, email } = req.body;
-
-    // Check if username or email already exists
-    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
-    if (existingUser) {
-      return res
-        .status(409)
-        .json({ message: "Username or email already exists" });
-    }
-
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Create a new user
-    const newUser = new User({
-      username,
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      username: req.body.username,
+      email: req.body.email,
       password: hashedPassword,
-      email,
     });
-
-    // Save the user to the database
-    await newUser.save();
-
-    return res.status(201).json({ message: "User signup successful" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error" });
+    res.redirect("/login");
+  } catch (e) {
+    res.redirect("/register");
   }
+  console.log(users);
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+app.delete("/logout", (req, res) => {
+  req.logOut(function (err) {
+    if (err) {
+      console.error(err);
+    }
+    res.redirect("/login");
+  });
 });
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect("/login");
+}
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+
+  next();
+}
+
+app.listen(port);
